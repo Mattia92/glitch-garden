92237ca472d3a078cfd12699b0df474f30eb269e
Mattia Crippa <mattia.crippa92@gmail.com>
2020-08-14 18:39:04 +0200

    Add options, difficulty and save settings

--- Assets/Scripts/DamageCollider.cs
+++ Assets/Scripts/DamageCollider.cs
@@ -6,5 +6,6 @@ public class DamageCollider : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision) {
         FindObjectOfType<LivesDisplay>().TakeLife();
+        Destroy(collision.gameObject);
     }
 }
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+
+    [SerializeField] Slider difficultySlider;
+    [SerializeField] float defaultDifficulty = 0f;
+
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        difficultySlider.value = PlayerPrefsController.GetDifficulty();
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer) {
+            musicPlayer.SetVolume(volumeSlider.value);
+        } else {
+            Debug.LogWarning("No music player found");
+        }
+    }
+
+    public void SetDefaults() {
+        difficultySlider.value = defaultDifficulty;
+        volumeSlider.value = defaultVolume;
+    }
+
+    public void SaveAndExit() {
+        PlayerPrefsController.SetDifficulty(difficultySlider.value);
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+}
